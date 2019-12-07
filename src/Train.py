5e8b1258318e36df6ba1618918783834f6bf5dec
dragoonc <yinchao508@mail.nwpu.edu.cn>
2019-12-08 07:44:25 +0800

    Create Train.py

---
+++ src/Train.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# coding=UTF-8 
+
+"""
+@file: Train.py
+@time: 2019/11/30
+@desc: Defining training and evaluating function without updating any parameters
+"""
+import Model
+import Seq2Seq
+import torch
+import torch.nn as nn
+
+def train(model, iterator, optimizer, criterion, clip):
+    model.train()
+    epoch_loss = 0
+
+    for i, batch in enumerate(iterator):
+        src = batch.src
+        trg = batch.trg
+        optimizer.zero_grad()
+        output = model(src, trg)
+
+        # trg = [trg sent len, batch size]
+        # output = [trg sent len, batch size, output dim]
+        # .view函数将多维展开成一维，方便loss函数计算。并且output[1:]和trg[1:]删去了<sos>token，因为无需计算<sos>token的loss
+        output = output[1:].view(-1, output.shape[-1])
+        trg = trg[1:].view(-1)
+
+        # trg = [(trg sent len - 1) * batch size]
+        # output = [(trg sent len - 1) * batch size, output dim]
+
+        loss = criterion(output, trg)
+
+        loss.backward()
+
+        # 修剪梯度防止梯度爆炸
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+
+        optimizer.step()
+
+        epoch_loss += loss.item()
+
+    return epoch_loss / len(iterator)
+
+
+def evaluate(model, iterator, criterion):
+    model.eval()
+    epoch_loss = 0
+
+    with torch.no_grad():
+        for i, batch in enumerate(iterator):
+            src = batch.src
+            trg = batch.trg
+
+            output = model(src, trg, 0)  # turn off teacher forcing
+
+            # trg = [trg sent len, batch size]
+            # output = [trg sent len, batch size, output dim]
+
+            output = output[1:].view(-1, output.shape[-1])
+            trg = trg[1:].view(-1)
+
+            # trg = [(trg sent len - 1) * batch size]
+            # output = [(trg sent len - 1) * batch size, output dim]
+
+            loss = criterion(output, trg)
+
+            epoch_loss += loss.item()
+
+    return epoch_loss / len(iterator)
