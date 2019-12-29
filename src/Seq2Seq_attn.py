11b116301e9b58d514b7117a28ed8a9986d77df6
dragoonc <yinchao508@mail.nwpu.edu.cn>
2019-12-30 07:27:10 +0800

    Create Seq2Seq_attn.py

---
+++ src/Seq2Seq_attn.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# coding=UTF-8 
+
+"""
+@file: Seq2Seq.py
+@time: 2019/11/30
+@desc: The Seq2Seq network combines encoder and decoder networks to generate context vectors and predict target language sentences
+"""
+from Model_attention import *
+import torch
+import random
+import torch.nn as nn
+
+class attn_Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder, device):
+        super(attn_Seq2Seq, self).__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+
+        # 为了方便处理，所以将encoder和decoder的隐状态维度和层数设置成一样的值
+        assert encoder.enc_hid_dim == decoder.dec_hid_dim, \
+            "Hidden dimensions of encoder and decoder must be equal!"
+        assert encoder.n_layers == decoder.n_layers, \
+            "Encoder and decoder must have equal number of layers!"
+
+
+    def forward(self, src, trg, teacher_forcing_ratio=0.5):
+        # src = [src sent len, batch size]
+        # trg = [trg sent len, batch size]
+        # teacher_forcing_ratio is a probability to use teacher forcing
+        # e.g. if teacher_forcing_ratio is 0.75 we use ground-truth inputs 75% of the time
+
+        batch_size = src.shape[1]
+        max_len = trg.shape[0]
+        trg_vocab_size = self.decoder.output_dim
+
+        # tensor to store decoder outputs
+        outputs = torch.zeros(max_len, batch_size, trg_vocab_size).to(self.device)
+        # last hidden state of the encoder is used as the initial hidden state of the decoder
+        encoder_output, hidden = self.encoder(src) #@@@@@@@
+        ###### outputs, hidden, cell = self.encoder(src)
+
+        # first input to the decoder is the <sos> tokens
+        input = trg[0, :]
+
+        for t in range(1, max_len):
+            prediction, output, hidden = self.decoder(input, hidden, encoder_output) #@@@@@@@
+            ##### output, hidden, cell = self.decoder(input, hidden, cell)
+            outputs[t] = output
+            teacher_force = random.random() < teacher_forcing_ratio
+            top1 = output.max(1)[1]
+            input = (trg[t] if teacher_force else top1)
+
+        return outputs
